ac80ddcff1618185da16e73c32e5963d6727c797
alagha-go <abdihakimosman1480@gmail.com>
2022-05-05 16:53:42 +0000

    added some inevitable fields to types Movie, Season and Episode

--- lib/types/movies.go
+++ lib/types/movies.go
@@ -11,6 +11,7 @@ type Movie struct {
 	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
 	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
 	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
     PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
     ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
     Released									*time.Time								`json:"released,omitempty" bson:"released"`
@@ -28,6 +29,7 @@ type Movie struct {
 
 type Season struct {
 	Name										string									`json:"name,omitempty" bson:"name,omitempty"`
+    Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
     Code										string									`json:"code,omitempty" bson:"code,omitempty"`
 	Episodes									[]Episode								`json:"episodes,omitempty" bson:"episodes,omitempty"`
 }
@@ -35,6 +37,8 @@ type Season struct {
 
 type Episode struct {
 	Name										string									`json:"name,omitempty" bson:"name,omitempty"`
+    Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
     Code										string									`json:"code,omitempty" bson:"code,omitempty"`
     Urls										[]string								`json:"urls,omitempty" bson:"urls,omitempty"`
 	Server										Server									`json:"server,omitempty" bson:"server,omitempty"`
