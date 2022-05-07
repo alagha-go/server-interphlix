3e5e3270053fdf8f1fe8eec992a7b3ba78371e65
alagha-go <abdihakimosman1480@gmail.com>
2022-05-07 17:41:23 +0000

    connecting the server to the databases

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"interphlix/lib/handler"
+	"interphlix/lib/variables"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -10,8 +16,30 @@ var (
 )
 
 func main() {
-
+	ConnectToDB()
 	go handler.Main()
 
 	http.ListenAndServe(PORT, handler.Router)
+}
+
+/// connect to both the local and remote mongodb databases
+func ConnectToDB() {
+	var err error
+	secret := variables.LoadSecret()
+	LocalClientOptions := options.Client().ApplyURI(secret.LocalDBUrl)
+	RemoteClientOptions := options.Client().ApplyURI(secret.RemoteDBUrl)
+	ctx := context.Background()
+	
+	variables.LocalClient, err = mongo.Connect(ctx, LocalClientOptions)
+	HandlError(err)
+	variables.RemoteClient, err = mongo.Connect(ctx, RemoteClientOptions)
+	HandlError(err)
+}
+
+
+// handle errors that need the program to exit
+func HandlError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
