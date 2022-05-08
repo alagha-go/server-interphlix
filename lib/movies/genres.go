6e38fd13a62fca48e0174c7eefbc322eaf8ffbb7
alagha-go <abdihakimosman1480@gmail.com>
2022-05-08 18:30:44 +0000

    added function to listen forchanges in the Genres collection

--- lib/movies/genres.go
+++ lib/movies/genres.go
@@ -17,3 +17,27 @@ func LoadGenres() {
 	err = cursor.All(ctx, &Genres)
 	variables.HandleError(err, "LoadGenres", "error while decoding genres from the cursor")
 }
+
+
+func ListenGenres() {
+	var genres []Genre
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Genres")
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	variables.HandleError(err, "ListenMovies", "error while listening for changes in Genres collection")
+	err = cursor.All(ctx, &genres)
+	variables.HandleError(err, "ListenMovies", "error while decoding Genres from the cursor")
+	for _, genre := range genres {
+		if genre.Exists() {
+			for genreIndex, Genre := range Genres {
+				if Genre.ID == genre.ID {
+					Genres[genreIndex] = genre
+				}
+			}
+		}else {
+			Genres = append(Genres, genre)
+		}
+	}
+	ListenGenres()
+}
--- lib/movies/main.go
+++ lib/movies/main.go
@@ -11,4 +11,5 @@ func Main() {
 	LoadMovies()
 	LoadGenres()
 	go ListenMovies()
+	go ListenGenres()
 }
