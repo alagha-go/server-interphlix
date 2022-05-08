de4e15e750e364ec5fbe3dd5f20115e9badacf3b
alagha-go <abdihakimosman1480@gmail.com>
2022-05-08 18:20:21 +0000

    function to listen for any changes in the movies collection

--- lib/movies/main.go
+++ lib/movies/main.go
@@ -8,5 +8,6 @@ var (
 
 
 func Main() {
-	
+	LoadMovies()
+	go ListenMovies()
 }
--- lib/movies/movies.go
+++ lib/movies/movies.go
@@ -17,3 +17,26 @@ func LoadMovies() {
 	err = cursor.All(ctx, &Movies)
 	variables.HandleError(err, "LoadMovies", "error while decoding movies from the cursor")
 }
+
+func ListenMovies() {
+	var movies []Movie
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Movies")
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	variables.HandleError(err, "ListenMovies", "error while listening for changes in movies collection")
+	err = cursor.All(ctx, &movies)
+	variables.HandleError(err, "ListenMovies", "error while decoding movies from the cursor")
+	for _, movie := range movies {
+		if movie.Exists() {
+			for movieIndex, Movie := range Movies {
+				if Movie.ID == movie.ID {
+					Movies[movieIndex] = movie
+				}
+			}
+		}else {
+			Movies = append(Movies, movie)
+		}
+	}
+	ListenMovies()
+}
