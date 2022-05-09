a70a569f0b11275d601ae3cd5773f2ce8b5017b8
alagha-go <abdihakimosman1480@gmail.com>
2022-05-09 08:02:28 +0000

    added endpoints to handle http requests

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -16,4 +16,8 @@ var (
 func Main() {
 	Router.HandleFunc("/", movies.GetHomeMovies)
 	Router.HandleFunc("/movies/upload", movies.UploadMovie)
+	Router.HandleFunc("/home", movies.GetHomeMovies)
+	Router.HandleFunc("/movies/addserver/{id}", movies.UploadMovie)
+	Router.HandleFunc("/movies/addurl/{id}/{url}", movies.UploadMovie)
+	Router.HandleFunc("/movies/setserver/{id}/{servername}/{serverid}", movies.UploadMovie)
 }
