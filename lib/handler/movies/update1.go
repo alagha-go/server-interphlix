e4ebb3e05665834a139030958bba19de76553ed7
alagha-go <abdihakimosman1480@gmail.com>
2022-05-09 07:57:22 +0000

    added http handler to handle func to update movie.Server

---
+++ lib/handler/movies/update1.go
@@ -0,0 +1,29 @@
+package movies
+
+import (
+	"interphlix/lib/movies"
+	"interphlix/lib/variables"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func SetServer(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	if req.Method != "PUT" &&  req.Method != "UPDATE" && req.Method != "PATCH"{
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	params := mux.Vars(req)
+	ID, err := primitive.ObjectIDFromHex(params["id"])
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "invalid id"}))
+		return
+	}
+	data, status := movies.SetServer(ID, params["servername"], params["serverid"])
+	res.WriteHeader(status)
+	res.Write(data)
+}
