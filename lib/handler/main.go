879878894bee4c71d6d99d77741ec9e6fdef593c
alagha-go <abdihakimosman1480@gmail.com>
2022-05-10 08:19:47 +0000

    added http handler to handle reload of all the servers

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"interphlix/lib/handler/movies"
+	"interphlix/lib/servers"
+	"interphlix/lib/variables"
+	"net/http"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -18,7 +21,13 @@ func Main() {
 	Router.HandleFunc("/", movies.GetHomeMovies)
 	Router.HandleFunc("/movies/upload", movies.UploadMovie)
 	Router.HandleFunc("/home", movies.GetHomeMovies)
+	Router.HandleFunc("/servers/reload", ReloadServers)
 	Router.HandleFunc("/movies/addserver/{id}", movies.UploadMovie)
 	Router.HandleFunc("/movies/addurl/{id}/{url}", movies.UploadMovie)
 	Router.HandleFunc("/movies/setserver/{id}/{servername}/{serverid}", movies.UploadMovie)
+}
+
+func ReloadServers(res http.ResponseWriter, req *http.Request) {
+	servers.ReloadServers()
+	res.Write(variables.JsonMarshal(`{"success": true}`))
 }
