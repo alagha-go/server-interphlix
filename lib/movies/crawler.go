8cbd772ee65672a2f3db0e0374b43c076f479e69
alagha-go <abdihakimosman1480@gmail.com>
2022-05-10 17:06:29 +0000

    collects the remaining servers that were missed by the crawler

---
+++ lib/movies/crawler.go
@@ -0,0 +1,47 @@
+package movies
+
+import "github.com/gocolly/colly"
+
+
+func (Movie *Movie) AddServers() {
+	if len(Movie.Servers) > 0 {
+		for _, Server := range Movie.Servers {
+			if Server.Name == "Vidcloud" {
+						collector := colly.NewCollector()
+
+						collector.OnHTML("#user_menu", func(element *colly.HTMLElement) {
+							servers := GetServers(element)
+							Movie.Servers = append(Movie.Servers, servers...)
+						})
+						collector.Visit(Server.Url)
+					}
+		}
+	}else {
+		for Sindex, Season := range Movie.Seasons {
+			for eindex, Episode := range Season.Episodes {
+				for _, Server := range Episode.Servers {
+					if Server.Name == "Vidcloud" {
+						collector := colly.NewCollector()
+
+						collector.OnHTML("#user_menu", func(element *colly.HTMLElement) {
+							servers := GetServers(element)
+							Movie.Seasons[Sindex].Episodes[eindex].Servers = append(Movie.Seasons[Sindex].Episodes[eindex].Servers, servers...)
+						})
+						collector.Visit(Server.Url)
+					}
+				}
+			}
+		}
+	}
+}
+
+func GetServers(element *colly.HTMLElement) []Server {
+	var Servers []Server
+    element.ForEach(".dl-site", func(_ int, element *colly.HTMLElement) {
+		var server Server
+		server.Name = element.ChildText(".site-name")
+		server.Url = element.ChildAttr("a", "href")
+		Servers = append(Servers, server)
+	})
+	return Servers
+}
