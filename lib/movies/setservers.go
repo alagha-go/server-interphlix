21528f09fe414f29338ce248a2eb8678fee0019e
alagha-go <abdihakimosman1480@gmail.com>
2022-05-11 17:19:29 +0000

    added function set server if available

---
+++ lib/movies/setservers.go
@@ -0,0 +1,26 @@
+package movies
+
+
+func (Movie *Movie) SetServer() {
+	if Movie.Type == "Movie" {
+		for _, Server := range Movie.Servers {
+			if Server.Name == "Streamlare" {
+				Movie.Server = &Server
+			}
+		}
+	}else if Movie.Type == "Tv-show" {
+		for Sindex, Season := range Movie.Seasons {
+			for Eindex := range Season.Episodes {
+				Movie.Seasons[Sindex].Episodes[Eindex].SetServer()
+			}
+		}
+	}
+}
+
+func (Episode *Episode) SetServer() {
+	for _, Server := range Episode.Servers {
+		if Server.Name == "Streamlare" {
+				Episode.Server = &Server
+		}
+	}
+}
