f17be0da9f793bdfed0b945f465a538967703a30
alagha-go <abdihakimosman1480@gmail.com>
2022-05-11 08:19:31 +0000

    added Package parameter to the GetErrors function

--- lib/variables/errors.go
+++ lib/variables/errors.go
@@ -35,6 +35,15 @@ func (log *Log) HandleError() {
 	}
 }
 
-func GetErrors() ([]byte, int) {
-	return JsonMarshal(Errors), 200
+func GetErrors(Package string) ([]byte, int) {
+	if Package == "" {
+		return JsonMarshal(Errors), 200
+	}
+	var Logs []Log
+	for _, Log := range Errors {
+		if Log.Package == Package {
+			Logs = append(Logs, Log)
+		}
+	}
+	return JsonMarshal(Logs), 200
 }
