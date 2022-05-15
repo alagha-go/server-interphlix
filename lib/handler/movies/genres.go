c2c8706a14ae4b081f51a64dd884a1578b46c3bb
alagha-go <abdihakimosman1480@gmail.com>
2022-05-15 08:38:37 +0000

    added function to get genres by type

--- lib/handler/movies/genres.go
+++ lib/handler/movies/genres.go
@@ -4,6 +4,8 @@ import (
 	"interphlix/lib/movies/genres"
 	"interphlix/lib/variables"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 
@@ -15,4 +17,24 @@ func GetAllGenres(res http.ResponseWriter, req *http.Request) {
 	}
 	res.WriteHeader(200)
 	res.Write(variables.JsonMarshal(genres.Genres))
+}
+
+
+func GetGenreByType(res http.ResponseWriter, req *http.Request) {
+	var Genres []genres.Genre
+	res.Header().Set("content-type", "application/json")
+	if req.Method != "GET" {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	Type := mux.Vars(req)["type"]
+	for index := range genres.Genres {
+		for tindex := range genres.Genres[index].Types {
+			if genres.Genres[index].Types[tindex] == Type{
+				Genres = append(Genres, genres.Genres[index])
+			}
+		}
+	}
+	res.WriteHeader(200)
+	res.Write(variables.JsonMarshal(Genres))
 }
