b9a4b6695cfec9d6b85bbab1d052a453ec94e715
alagha-go <abdihakimosman1480@gmail.com>
2022-05-15 08:44:06 +0000

    added function to handle request to get movies by Genre

--- lib/handler/movies/home.go
+++ lib/handler/movies/home.go
@@ -2,7 +2,10 @@ package movies
 
 import (
 	"interphlix/lib/movies"
+	"interphlix/lib/variables"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 
@@ -15,4 +18,24 @@ func GetHomeMovies(res http.ResponseWriter, req *http.Request) {
 	data, status := movies.GetMovies()
 	res.WriteHeader(status)
 	res.Write(data)
+}
+
+
+func GetMoviesByGenre(res http.ResponseWriter, req *http.Request) {
+	var Movies []movies.Movie
+	res.Header().Set("content-type", "application/json")
+	if req.Method != "GET" {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	genre := mux.Vars(req)["genre"]
+	for _, Movie := range movies.Movies {
+		for _, Genre := range Movie.Genres {
+			if genre == Genre {
+				Movies = append(Movies, Movie)
+			}
+		}
+	}
+	res.WriteHeader(200)
+	res.Write(variables.JsonMarshal(Movies))
 }
