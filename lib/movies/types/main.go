fd70569b4754e47eb3d5a6de4b4ada69ed14c99a
alagha-go <abdihakimosman1480@gmail.com>
2022-05-15 07:23:36 +0000

    created function to load all the types from the database

--- lib/movies/types/main.go
+++ lib/movies/types/main.go
@@ -1,11 +1,28 @@
 package types
 
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 
 var (
-
+	Types []Type
 )
 
 
 func Main() {
-	
+
+}
+
+
+func LoadTypes() {
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Types")
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	variables.HandleError(err, "types", "LoadTypes", "error while loading data from the database")
+	cursor.All(ctx, &Types)
 }
