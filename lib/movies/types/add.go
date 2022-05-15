5f9b8d373f127c6bda7cb527dd592e5ad173147c
alagha-go <abdihakimosman1480@gmail.com>
2022-05-15 07:33:43 +0000

    created functions to add type to the database

---
+++ lib/movies/types/add.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func AddType(title string) {
+	if TypeExists(title) {
+		return
+	}
+	var Type Type
+	Type.ID = primitive.NewObjectID()
+	Type.Type = title
+	inserted := Type.AddToDB()
+	if inserted {
+		Types = append(Types, Type)
+	}
+}
+
+
+func (Type *Type) AddToDB() bool {
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Types")
+
+	_, err := collection.InsertOne(ctx, Type)
+	if err != nil {
+		variables.HandleError(err, "types", "AddToDB", "error while adding type to the database")
+		return false
+	}
+	return true
+}
