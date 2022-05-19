7875d17b4e153b98c735e224c5efba0d7a525be7
alagha-go <abdihakimosman1480@gmail.com>
2022-05-19 18:30:19 +0000

    created function to listen for changes in movies collection and update my data

---
+++ lib/movies/listener.go
@@ -0,0 +1,51 @@
+package movies
+
+import (
+	"context"
+	"encoding/json"
+	"interphlix/lib/variables"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+func Listener() {
+	go ListenForMoviesCollection()
+}
+
+
+func ListenForMoviesCollection() {
+	collection := variables.Client.Database("Interphlix").Collection("Movies")
+
+	matchPipeline := bson.D{{"$match", bson.D{{"operationType", bson.D{{ "$in", bson.A{"insert", "update", "replace"} }}}}}}
+	projectPipeline := bson.D{{ "$project", bson.D{{"fullDocument", 1}}}}
+
+	opts := options.ChangeStream().SetMaxAwaitTime(2 * time.Second)
+	opts.SetFullDocument("updateLookup")
+
+	stream, err := collection.Watch(context.TODO(), mongo.Pipeline{matchPipeline, projectPipeline}, opts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for stream.Next(context.TODO()) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
+			panic(err)
+		}
+		content := variables.JsonMarshal(data["fullDocument"])
+		var movie Movie
+		json.Unmarshal(content, &movie)
+		index, err := movie.GetIndex()
+		if err != nil {
+			Movies = append(Movies, movie)
+		}
+		Movies[index] = movie
+		println(movie.ID.Hex())
+	}
+	ListenForMoviesCollection()
+}
