a3f0dfe0d4d4b0849aef23bc2dc88827a4e53abf
alagha-go <abdihakimosman1480@gmail.com>
2022-05-21 19:27:08 +0000

    created Account struct

---
+++ lib/accounts/struct.go
@@ -0,0 +1,15 @@
+package accounts
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+
+type Account struct {
+	ID									primitive.ObjectID							`json:"_id,omitempty" bson:"_id,omitempty"`
+	GoogleID							string										`json:"id,omitempty" bson:"id,omitempty"`
+	Email								string										`json:"email,omitempty" bson:"email,omitempty"`
+	EmailVerified						bool										`json:"verified_email,omitempty" bson:"verified_email,omitempty"`
+	GivenName							string										`json:"given_name,omitempty" bson:"given_name,omitempty"`
+	FamilyName							string										`json:"family_name,omitempty" bson:"family_name,omitempty"`
+	Picture								string										`json:"picture,omitempty" bson:"picture,omitempty"`
+	Locale								string										`json:"locale,omitempty" bson:"locale,omitempty"`
+}
