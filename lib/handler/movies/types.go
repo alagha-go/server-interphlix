3c55462758b5071d4913c0e7661c6b3943ce9fae
alagha-go <abdihakimosman1480@gmail.com>
2022-05-22 08:23:01 +0000

    called function to validate request

--- lib/handler/movies/types.go
+++ lib/handler/movies/types.go
@@ -1,6 +1,7 @@
 package movies
 
 import (
+	"interphlix/lib/handler/accounts"
 	"interphlix/lib/movies/types"
 	"interphlix/lib/variables"
 	"net/http"
@@ -9,6 +10,12 @@ import (
 
 func GetTypes(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
 	res.WriteHeader(200)
 	res.Write(variables.JsonMarshal(types.Types))
 }
