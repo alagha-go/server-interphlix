df2583123896cbc34841fc158754bad4bf089ef5
alagha-go <abdihakimosman1480@gmail.com>
2022-05-22 07:34:22 +0000

    updated function to return login jwt cookie

--- lib/handler/accounts/redirect.go
+++ lib/handler/accounts/redirect.go
@@ -2,9 +2,13 @@ package accounts
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"interphlix/lib/accounts"
+	"interphlix/lib/variables"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"golang.org/x/oauth2"
 )
@@ -12,11 +16,29 @@ import (
 
 
 func LoginRedirect(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("content-type", "application/json")
+	var account accounts.Account
 	code := req.URL.Query().Get("code")
 	token := GetToken(code)
+	json.Unmarshal(GetUserInfo(token.AccessToken), &account)
+	data, status := accounts.CreateAccount(account)
+	if status != 200 {
+		res.WriteHeader(status)
+		res.Write(data)
+		return
+	}
+	json.Unmarshal(data, &account)
+	tokenString, status, err := GenerateToken(account)
+	if err != nil {
+		res.WriteHeader(status)
+		res.Write(variables.JsonMarshal(variables.Error{Error: err.Error()}))
+	}
+	http.SetCookie(res, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: time.Now().Add(120*time.Hour),
+	})
 	res.WriteHeader(200)
-	res.Write(GetUserInfo(token.AccessToken))
+	res.Write([]byte(fmt.Sprintf("<h1>Successfully loged in to Interphlix. Welcome %s</h1>", account.Name)))
 }
 
 
