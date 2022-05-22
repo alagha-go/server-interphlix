b63f5506a34e62a6c06a669b9676523eb9c91ff8
alagha-go <abdihakimosman1480@gmail.com>
2022-05-22 08:22:12 +0000

    called function to validate request

--- lib/handler/movies/genres.go
+++ lib/handler/movies/genres.go
@@ -1,6 +1,7 @@
 package movies
 
 import (
+	"interphlix/lib/handler/accounts"
 	"interphlix/lib/movies/genres"
 	"interphlix/lib/variables"
 	"net/http"
@@ -11,6 +12,12 @@ import (
 
 func GetAllGenres(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
 	res.WriteHeader(200)
 	res.Write(variables.JsonMarshal(genres.Genres))
 }
@@ -19,6 +26,12 @@ func GetAllGenres(res http.ResponseWriter, req *http.Request) {
 func GetGenreByType(res http.ResponseWriter, req *http.Request) {
 	var Genres []genres.Genre
 	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
 	Type := mux.Vars(req)["type"]
 	for index := range genres.Genres {
 		for tindex := range genres.Genres[index].Types {
