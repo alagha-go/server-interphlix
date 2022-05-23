88ee40db9de71ed526cba19cf82fadcb0bc1ed80
alagha-go <abdihakimosman1480@gmail.com>
2022-05-23 18:50:37 +0000

    added endpoint to get user files

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -39,6 +39,7 @@ func Main() {
 	Router.HandleFunc("/types", movies.GetTypes).Methods("GET")
 	Router.HandleFunc("/myaccount", accounts.GetMyAccount).Methods("GET")
 	Router.HandleFunc("/token/refresh", accounts.RenewToken).Methods("GET")
+	Router.HandleFunc("/files", accounts.GetMyFiles).Methods("GET")
 	Router.HandleFunc("/movies/{type}/{genre}", movies.GetMoviesByTypeAndGenre).Methods("GET")
 	Router.HandleFunc("/movies/deleteserver/{id}", movies.DeleteServer).Methods("DELETE")
 	Router.HandleFunc("/errors/{package}", Errors).Methods("GET")
