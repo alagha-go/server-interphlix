503f6069aadca29c31499848ae2fd0e3c38a1454
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 17:25:09 +0000

    created function to load all accounts

--- lib/accounts/main.go
+++ lib/accounts/main.go
@@ -1,6 +1,25 @@
 package accounts
 
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+
+var (
+	Accounts []Account
+)
 
 func Main() {
-	
+	LoadAccounts()
+}
+
+func LoadAccounts() {
+	collection := variables.Client.Database("Interphlix").Collection("Accounts")
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	variables.HandleError(err, "accounts", "LoadAccounts", "error while loading accounts from the database")
+	cursor.All(context.Background(), &Accounts)
 }
