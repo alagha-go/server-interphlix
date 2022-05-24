cd80f9d8d048085606bd34dc2e58ee350d28f371
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 18:01:03 +0000

    added Type field to account to differentiate customer from staff

--- lib/accounts/struct.go
+++ lib/accounts/struct.go
@@ -9,6 +9,7 @@ import (
 type Account struct {
 	ID									primitive.ObjectID							`json:"_id,omitempty" bson:"_id,omitempty"`
 	GoogleID							string										`json:"id,omitempty" bson:"id,omitempty"`
+	Type								string										`json:"type,omitempty" bson:"type,omitempty"`
 	Email								string										`json:"email,omitempty" bson:"email,omitempty"`
 	EmailVerified						bool										`json:"verified_email,omitempty" bson:"verified_email,omitempty"`
 	Name								string										`json:"name,omitempty" bson:"name,omitempty"`
