d7a3333cbf4d5768356263ea0ab81e596d37d0f5
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 18:43:22 +0000

    created a listener to listen for changes in the Casts collection

---
+++ lib/movies/casts/listener.go
@@ -0,0 +1,49 @@
+package casts
+
+import (
+	"context"
+	"encoding/json"
+	"interphlix/lib/variables"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+func Listener() {
+
+}
+
+func ListenForCastsCollection() {
+	collection := variables.Client.Database("Interphlix").Collection("Casts")
+
+	matchPipeline := bson.D{{"$match", bson.D{{"operationType", bson.D{{ "$in", bson.A{"insert", "update", "replace"} }}}}}}
+	projectPipeline := bson.D{{ "$project", bson.D{{"fullDocument", 1}}}}
+
+	opts := options.ChangeStream().SetMaxAwaitTime(2 * time.Second)
+	opts.SetFullDocument("updateLookup")
+
+	stream, err := collection.Watch(context.TODO(), mongo.Pipeline{matchPipeline, projectPipeline}, opts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for stream.Next(context.TODO()) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
+			panic(err)
+		}
+		content := variables.JsonMarshal(data["fullDocument"])
+		var cast Cast
+		json.Unmarshal(content, &cast)
+		index, err := cast.Index()
+		if err != nil {
+			Casts = append(Casts, cast)
+		}
+		Casts[index] = cast
+	}
+	ListenForCastsCollection()
+}
