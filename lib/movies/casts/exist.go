55b6ae2f3244201655fca524f1b266831c581a0d
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 18:36:12 +0000

    created function to get cast's index in the list

--- lib/movies/casts/exist.go
+++ lib/movies/casts/exist.go
@@ -1,5 +1,7 @@
 package casts
 
+import "errors"
+
 
 func CastExists(name string) bool {
 	for _, Cast := range Casts {
@@ -8,4 +10,14 @@ func CastExists(name string) bool {
 		}
 	}
 	return false
+}
+
+
+func (cast *Cast) Index() (int, error) {
+	for index, Cast := range Casts {
+		if Cast.ID == cast.ID {
+			return index, nil
+		}
+	}
+	return 0, errors.New("cast does not exist")
 }
