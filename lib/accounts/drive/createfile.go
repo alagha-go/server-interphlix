2003f66ca29715f3cfa7e2e6ee289930ba63d3d2
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 16:42:02 +0000

    added function to check if file exists before creating it

--- lib/accounts/drive/createfile.go
+++ lib/accounts/drive/createfile.go
@@ -2,9 +2,11 @@ package drive
 
 import (
 	"context"
+	"encoding/json"
 	"interphlix/lib/accounts"
 	"interphlix/lib/variables"
 	"io/ioutil"
+	"net/http"
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -13,25 +15,42 @@ import (
 
 
 func CreateFile(account accounts.Account, filename string) ([]byte, int) {
+	fileExists := FileExist(account, filename)
+	if fileExists {
+		return variables.JsonMarshal(variables.Error{Error: "file already exists"}), http.StatusConflict
+	}
 	ctx := context.Background()
 	secretBody, err := ioutil.ReadFile("./secret1.json")
 	if err != nil {
-		return variables.JsonMarshal(variables.Error{Error: err.Error()}), 500
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
 	config, err := google.ConfigFromJSON(secretBody, drive.DriveFileScope)
 	if err != nil {
-		return variables.JsonMarshal(variables.Error{Error: err.Error()}), 500
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
 	client := config.Client(context.Background(), account.Token)
 
 	driveService, err := drive.NewService(ctx, option.WithHTTPClient(client))
     if err != nil {
-		return variables.JsonMarshal(variables.Error{Error: err.Error()}), 500
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
 	file := &drive.File{Name: filename}
 	file, err = driveService.Files.Create(file).Do()
 	if err != nil {
-		return variables.JsonMarshal(variables.Error{Error: err.Error()}), 500
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
-	return variables.JsonMarshal(file), 200
+	return variables.JsonMarshal(file), http.StatusOK
+}
+
+
+func FileExist(account accounts.Account, filename string) bool {
+	var files []drive.File
+	data, _ := GetFiles(account)
+	json.Unmarshal(data, &files)
+	for _, file := range files {
+		if filename == file.Name {
+			return true
+		}
+	}
+	return false
 }
