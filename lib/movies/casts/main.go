76c7aa1a7a906f4f938ad24534e4e2728f1e08e2
alagha-go <abdihakimosman1480@gmail.com>
2022-05-24 18:30:57 +0000

    created function to load casts

--- lib/movies/casts/main.go
+++ lib/movies/casts/main.go
@@ -1,6 +1,26 @@
 package casts
 
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+
+var (
+	Casts []Cast
+)
 
 func Main() {
-	
+	LoadCasts()
+}
+
+
+func LoadCasts() {
+	collection := variables.Client.Database("Interphlix").Collection("Casts")
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	variables.HandleError(err, "casts", "LoadCasts", "error while getting casts from the database")
+	cursor.All(context.Background(), &Casts)
 }
