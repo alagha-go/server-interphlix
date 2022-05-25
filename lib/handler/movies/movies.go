4dd6bac623b000e4f99216fea5691ed1611134ac
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 08:48:20 +0000

    updated functions to get movies

--- lib/handler/movies/movies.go
+++ lib/handler/movies/movies.go
@@ -10,7 +10,6 @@ import (
 )
 
 func GetMoviesByTypeAndGenre(res http.ResponseWriter, req *http.Request) {
-	var Movies []movies.Movie
 	res.Header().Set("content-type", "application/json")
 	valid := accounts.ValidateRequest(req)
 	if !valid {
@@ -21,16 +20,8 @@ func GetMoviesByTypeAndGenre(res http.ResponseWriter, req *http.Request) {
 	Type := mux.Vars(req)["type"]
 	Genre := mux.Vars(req)["genre"]
 
-	for _, Movie := range movies.Movies {
-		if Movie.Type == Type {
-			for _, genre := range Movie.Genres {
-				if genre == Genre {
-					Movies = append(Movies, Movie)
-				}
-			}
-		}
-	}
+	data, status := movies.GetMoviesByGenreAndType(Type, Genre)
 
-	res.WriteHeader(200)
-	res.Write(variables.JsonMarshal(Movies))
+	res.WriteHeader(status)
+	res.Write(data)
 }
