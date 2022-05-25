797bfadb34942f1b427942565f3cf54a1d593840
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 16:48:22 +0000

    removed functions with no use

--- lib/movies/types/add.go
+++ lib/movies/types/add.go
@@ -20,19 +20,6 @@ func AddType(title string) {
 	variables.HandleError(err, "types", "AddType", "error while inserting type to the remote database")
 }
 
-
-func (Type *Type) AddToDB() bool {
-	ctx := context.Background()
-	collection := variables.Client.Database("Interphlix").Collection("Types")
-
-	_, err := collection.InsertOne(ctx, Type)
-	if err != nil {
-		variables.HandleError(err, "types", "AddToDB", "error while adding type to the database")
-		return false
-	}
-	return true
-}
-
 func (Type *Type) AddToLocalDB() {
 	if TypeExists(Type.Type) {
 		return
