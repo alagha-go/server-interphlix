8cf742eafde66528a2a71206422dffb16439eb77
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 07:27:31 +0000

    created function to add movie to the local database

---
+++ lib/movies/addmovietodb.go
@@ -0,0 +1,28 @@
+package movies
+
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+
+func (movie *Movie) AddToDB() {
+	var dbmovie Movie
+	ctx := context.Background()
+	collection := variables.Client1.Database("Interphlix").Collection("Movies")
+
+	err := collection.FindOne(ctx, bson.M{"_id": movie.ID}).Decode(&dbmovie)
+	if err != nil {
+		collection.InsertOne(ctx, movie)
+		return
+	}
+	filter := bson.M{
+		"_id": bson.M{
+			"$eq": movie.ID, // check if bool field has value of 'false'
+		},
+	}
+	update := bson.M{"$set": movie}
+	collection.UpdateOne(ctx, filter, update)
+}
