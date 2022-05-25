ec348d43da50c32d3646565878d0f5cb4a4bbf35
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 07:39:58 +0000

    created function to load movies from the remote database to the local mongodb

--- lib/movies/main.go
+++ lib/movies/main.go
@@ -5,6 +5,9 @@ import (
 	"interphlix/lib/movies/casts"
 	"interphlix/lib/movies/genres"
 	"interphlix/lib/movies/types"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
@@ -21,5 +24,17 @@ func Main() {
 
 
 func LoadMoviesFromDB() {
-	
+	var documents []interface{}
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Movies")
+	collection1 := variables.Client1.Database("Interphlix").Collection("Movies")
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "movies", "LoadMoviesFromDB", "error while getting movies from the remote database")
+		return
+	}
+	cursor.All(ctx, &documents)
+	collection1.Drop(ctx)
+	_, err = collection1.InsertMany(ctx, documents)
+	variables.HandleError(err, "movies", "LoadMoviesFromDB", "error while inserting movies to the local database")
 }
