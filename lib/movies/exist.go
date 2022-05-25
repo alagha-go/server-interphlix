88d87b07aea46f8bf08c1f888ff7db18d6546ba9
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 07:46:32 +0000

    modified function to check if movie exists

--- lib/movies/exist.go
+++ lib/movies/exist.go
@@ -1,30 +1,30 @@
 package movies
 
 import (
+	"context"
+	"interphlix/lib/variables"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 /// check if a movie is in the Movies list
 func (movie *Movie) Exists() bool {
-	for _, Movie := range Movies {
-		if movie.Code == Movie.Code && movie.Title == Movie.Title {
-			return true
-		}
+	var Movie Movie
+	collection := variables.Client1.Database("Interphlix").Collection("Movies")
+
+	err := collection.FindOne(context.Background(), bson.M{"code": movie.Code}).Decode(&Movie)
+	if err != nil {
+		return false
 	}
-	return false
+	return true
 }
 
 
 
 /// find a specific movie from the Movies list
 func FindMovie(ID primitive.ObjectID) Movie {
-	for _, Movie := range Movies {
-		if Movie.ID == ID {
-			return Movie
-		}
-	}
-	return Movie{}
+	
 }
 
 func (Movie *Movie) Valid() bool {
