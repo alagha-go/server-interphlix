6a8f678dfd83413b9db81bb8e24155c586e7153f
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 08:24:46 +0000

    updated function to get home movies

--- lib/movies/recommend.go
+++ lib/movies/recommend.go
@@ -9,11 +9,32 @@ import (
 )
 
 
-func GetMovies() ([]byte, int) {
+func GetMovies(index int) ([]byte, int) {
+	var length int
 	var Movies []Movie
 	collection := variables.Client1.Database("Interphlix").Collection("Movies")
 	cursor, err := collection.Find(context.Background(), bson.M{})
 	variables.HandleError(err, "movies", "GetMovies", "error while getting movies from the local database")
 	cursor.All(context.Background(), &Movies)
-	return variables.JsonMarshal(Movies), http.StatusOK
+	if index == 0 {
+		length = 30
+	}else if index < 0{
+		length = 0
+	}else {
+		length = (index*10)+30
+	}
+	if length == 0 {
+		return variables.JsonMarshal(variables.Error{Error: "invalid index"}), http.StatusBadRequest
+	}else if length == 30 {
+		return variables.JsonMarshal(Movies[:30]), http.StatusOK
+	}else {
+		startIndex := 30+(index*10)
+		if startIndex > len(Movies) {
+			return []byte(`{}`), http.StatusNoContent
+		}else if length > len(Movies) {
+			return variables.JsonMarshal(Movies[startIndex:]), http.StatusOK
+		}else {
+			return variables.JsonMarshal(Movies[startIndex:length]), http.StatusOK
+		}
+	}
 }
