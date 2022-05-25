e8b9790399bd1ff831fd40891543ae1b3e6e1c0b
alagha-go <abdihakimosman1480@gmail.com>
2022-05-25 08:00:24 +0000

    modified function to loadGenres

--- lib/movies/genres/genres.go
+++ lib/movies/genres/genres.go
@@ -9,11 +9,21 @@ import (
 
 
 func LoadGenres() {
+	var documents []interface{}
 	ctx := context.Background()
-	collection := variables.Client.Database("Interphlix").Collection("Genres")
+	collection1 := variables.Client.Database("Interphlix").Collection("Genres")
+	collection := variables.Client1.Database("Interphlix").Collection("Genres")
 
-	cursor, err := collection.Find(ctx, bson.M{})
-	variables.HandleError(err, "genres", "LoadGenres", "error while getting genres from the Database")
-	err = cursor.All(ctx, &Genres)
-	variables.HandleError(err, "genres", "LoadGenres", "error while decoding genres from the cursor")
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "genres", "LoadGenres", "error while getting genres from the Database")
+		return
+	}
+	err = cursor.All(ctx, &documents)
+	if err != nil {
+		variables.HandleError(err, "genres", "LoadGenres", "error while decoding genres from the cursor")
+		return
+	}
+	collection.Drop(ctx)
+	collection.InsertMany(ctx, documents)
 }
