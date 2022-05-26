88572da0ec5fdd69af20eb223580672197517edb
alagha-go <abdihakimosman1480@gmail.com>
2022-05-26 07:12:46 +0000

    moved db.go to the variables package

---
+++ lib/variables/db.go
@@ -0,0 +1,52 @@
+package variables
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+func ConnectToRemoteDB1() {
+	secret := LoadSecret()
+	clientOptions := options.Client().
+		ApplyURI(secret.RemoteDBUrl)
+	ctx := context.Background()
+	
+	client, err := mongo.Connect(ctx, clientOptions)
+	Client = client
+	handleerror(err)
+}
+
+
+func ConnectRemotedDB2() {
+	secret := LoadSecret()
+	clientOptions := options.Client().
+		ApplyURI(secret.Remote2DBUrl)
+	ctx := context.Background()
+	
+	client, err := mongo.Connect(ctx, clientOptions)
+	Client2 = client
+	handleerror(err)
+}
+
+func ConnectLocalDB() {
+	secret := LoadSecret()
+	clientOptions := options.Client().
+		ApplyURI(secret.LocalDBUrl)
+	ctx := context.Background()
+	
+	client, err := mongo.Connect(ctx, clientOptions)
+	Client1 = client
+	handleerror(err)
+	println(Client1.ListDatabaseNames(context.Background(), bson.M{}))
+}
+
+func handleerror(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
