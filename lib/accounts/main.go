933f2d99ced66a2832cd29379ee775f578f1337e
alagha-go <abdihakimosman1480@gmail.com>
2022-05-26 07:38:30 +0000

    drop the collection before inserting data

--- lib/accounts/main.go
+++ lib/accounts/main.go
@@ -24,5 +24,6 @@ func LoadAccounts() {
 	cursor, err := collection1.Find(context.Background(), bson.M{})
 	variables.HandleError(err, "accounts", "LoadAccounts", "error while loading accounts from the database")
 	cursor.All(context.Background(), &documents)
+	collection.Drop(context.Background())
 	collection.InsertMany(context.Background(), documents)
 }
