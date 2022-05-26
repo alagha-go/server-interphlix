6507ee60b50aea698c0ec1ffd56aee2a3e2c13fc
alagha-go <abdihakimosman1480@gmail.com>
2022-05-26 07:23:25 +0000

    created function to get all accounts

--- lib/accounts/getaccount.go
+++ lib/accounts/getaccount.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"interphlix/lib/variables"
+	"net/http"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -20,4 +21,20 @@ func GetAccount(ID primitive.ObjectID) (Account, error) {
 		return account, errors.New("account not found")
 	}
 	return account, nil
+}
+
+
+
+func GetAccounts() ([]byte, int) {
+	var Accounts []Account
+	collection := variables.Client1.Database("Interphlix").Collection("Accounts")
+	ctx := context.Background()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "accounts", "GetAccounts", "error while getting data from the local database")
+		return variables.JsonMarshal(variables.Error{Error: "could not get accounts"}), http.StatusInternalServerError
+	}
+	cursor.All(ctx, &Accounts)
+	return variables.JsonMarshal(Accounts), http.StatusOK
 }
