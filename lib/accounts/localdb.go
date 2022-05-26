4948c11baeede46dc0f514d8c18fb119080d223e
alagha-go <abdihakimosman1480@gmail.com>
2022-05-26 07:37:51 +0000

    created function to update account to the local database

--- lib/accounts/localdb.go
+++ lib/accounts/localdb.go
@@ -20,3 +20,19 @@ func (account *Account) AddToDB() {
 	_, err = collection.InsertOne(ctx, account)
 	variables.HandleError(err, "accounts", "aacount.AddToDB", "error while inserting account to the local database")
 }
+
+
+func (account *Account) Update() {
+	ctx := context.Background()
+	collection := variables.Client1.Database("Interphlix").Collection("Accounts")
+
+	filter := bson.M{
+        "_id": bson.M{
+            "$eq": account.ID, // check if bool field has value of 'false'
+        },
+    }
+	update := bson.M{"$set": account}
+
+	_, err := collection.UpdateOne(ctx, filter, update)
+	variables.HandleError(err, "accounts", "account.Update", "error while updating account to the local database")
+}
