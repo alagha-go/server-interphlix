6dcb647dce98ea7ca33a01b7786e5ace2445c568
alagha-go <abdihakimosman1480@gmail.com>
2022-05-26 07:19:00 +0000

    updated function to get account

--- lib/accounts/getaccount.go
+++ lib/accounts/getaccount.go
@@ -1,21 +1,23 @@
 package accounts
 
 import (
+	"context"
 	"errors"
+	"interphlix/lib/variables"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
 func GetAccount(ID primitive.ObjectID) (Account, error) {
-	for _, account := range Accounts {
-		if account.ID == ID {
-			if account.Deleted {
-				return Account{}, errors.New("account does not exist")
-			}else {
-				return account, nil
-			}
-		}
+	var account Account
+	collection := variables.Client1.Database("Interphlix").Collection("Accounts")
+	ctx := context.Background()
+
+	err := collection.FindOne(ctx, bson.M{"_id": ID}).Decode(&account)
+	if err != nil {
+		return account, errors.New("account not found")
 	}
-	return Account{}, errors.New("account does not exist")
+	return account, nil
 }
