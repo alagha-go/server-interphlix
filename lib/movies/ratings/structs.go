68dc219448fd616fbc1cd2f05a1d10cb8e3a55b5
alagha-go <abdihakimosman1480@gmail.com>
2022-05-27 07:49:54 +0000

    removed the user field in the Rate struct since account should have no users

--- lib/movies/ratings/structs.go
+++ lib/movies/ratings/structs.go
@@ -10,7 +10,6 @@ import (
 type Rate struct {
 	ID									primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
 	AccountID							primitive.ObjectID								`json:"account-id,omitempty" bson:"account-id,omitempty"`
-	UserID								primitive.ObjectID								`json:"user-id,omitempty" bson:"user-id,omitempty"`
 	Stars								int												`json:"stars,omitempty" bson:"stars,omitempty"`
 	Review								string											`json:"review,omitempty" bson:"review,omitempty"`
 	TimeRated							time.Time										`json:"time-rated,omitempty" bson:"time-rated,omitempty"`
