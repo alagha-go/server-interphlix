97668317d979c9a00f7dbb326138eec9ef12b54c
alagha-go <abdihakimosman1480@gmail.com>
2022-05-27 08:49:47 +0000

    created function to check if rate exists in the database

--- lib/movies/ratings/localdb.go
+++ lib/movies/ratings/localdb.go
@@ -3,6 +3,8 @@ package ratings
 import (
 	"context"
 	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
@@ -17,4 +19,14 @@ func (Rate *Rate) AddToDB() {
 
 	_, err := collection.InsertOne(ctx, Rate)
 	variables.HandleError(err, "ratings", "Rate.AddToDB", "error while inserting rate to the local db")
+}
+
+
+func (rate *Rate) ExistsByID() bool {
+	var Rate Rate
+	ctx := context.Background()
+	collection := variables.Client1.Database("Interphlix").Collection("Ratings")
+
+	err := collection.FindOne(ctx, bson.M{"_id": rate.ID}).Decode(&Rate)
+	return err == nil
 }
