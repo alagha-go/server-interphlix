cff8e60c3926940c4970660b96865861bb36ab07
alagha-go <abdihakimosman1480@gmail.com>
2022-05-27 08:57:51 +0000

    created function to ratings from the remote database

--- lib/movies/ratings/main.go
+++ lib/movies/ratings/main.go
@@ -1,6 +1,31 @@
 package ratings
 
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 
 func Main() {
+	LoadRatings()
 	Listener()
+}
+
+
+func LoadRatings() {
+	var documents []interface{}
+	ctx := context.Background()
+	collection1 := variables.Client.Database("Interphlix").Collection("Ratings")
+	collection := variables.Client1.Database("Interphlix").Collection("Ratings")
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "ratings", "LoadRatings", "error while getting ratings from the remote database")
+		return
+	}
+	cursor.All(ctx, &documents)
+	_, err = collection.InsertMany(ctx, documents)
+	variables.HandleError(err, "ratings", "LoadRatings", "error while inserting ratings to the local database")
 }
