4763f46324f75190449bb65484cf5b623d6d1298
alagha-go <abdihakimosman1480@gmail.com>
2022-05-27 08:02:34 +0000

    added MovieID to the struct

--- lib/movies/ratings/structs.go
+++ lib/movies/ratings/structs.go
@@ -9,7 +9,8 @@ import (
 
 type Rate struct {
 	ID									primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
-	AccountID							primitive.ObjectID								`json:"account-id,omitempty" bson:"account-id,omitempty"`
+	AccountID							primitive.ObjectID								`json:"account_id,omitempty" bson:"account_id,omitempty"`
+	MovieID								primitive.ObjectID								`json:"movie_id,omitempty" bson:"movie_id,omitempty"`
 	Stars								int												`json:"stars,omitempty" bson:"stars,omitempty"`
 	Review								string											`json:"review,omitempty" bson:"review,omitempty"`
 	TimeRated							time.Time										`json:"time-rated,omitempty" bson:"time-rated,omitempty"`
