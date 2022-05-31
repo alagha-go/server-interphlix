6f930e86cf01513a7943ed516444128a5dabf302
alagha-go <abdihakimosman1480@gmail.com>
2022-05-31 19:14:30 +0000

    created socket server

--- lib/socket/main.go
+++ lib/socket/main.go
@@ -1,14 +1,38 @@
 package socket
 
 import (
-	socketio "github.com/rmolak/golang-socketio"
+	"log"
+	"net/http"
+
+	gosocketio "github.com/ambelovsky/gosf-socketio"
+	"github.com/ambelovsky/gosf-socketio/transport"
 )
 
 
-var (
-	Server = socketio.NewServer()
-)
+type Channel struct {
+	Channel string `json:"channel"`
+}
+
+type Message struct {
+	Id      int    `json:"id"`
+	Channel string `json:"channel"`
+	Text    string `json:"text"`
+}
+
 
 func Main() {
+	server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
 
+	server.On(gosocketio.OnConnection, func(channel *gosocketio.Channel) {
+		println("connected: ", channel.Id())
+	})
+	server.On(gosocketio.OnDisconnection, func(c *gosocketio.Channel) {
+		log.Println("Disconnected")
+	})
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/socket.io/", server)
+
+	log.Println("Starting server...")
+	log.Panic(http.ListenAndServe(":9000", serveMux))
 }
