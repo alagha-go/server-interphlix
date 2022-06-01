29d3c7f26a169559ca30e0153ac3e12cba839ba9
alagha-go <abdihakimosman1480@gmail.com>
2022-06-01 07:24:04 +0000

    created function to handle error

--- lib/socket/config.go
+++ lib/socket/config.go
@@ -2,6 +2,7 @@ package socket
 
 import (
 	"io/ioutil"
+	"log"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -12,4 +13,10 @@ func GetConfig() (*oauth2.Config, error) {
 	secretBody, err := ioutil.ReadFile("./secret1.json")
 	HandlError(err)
 	return google.ConfigFromJSON(secretBody, scopes...)
+}
+
+func HandlError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
