debd50c4289fb344fec52b48aa9cc54711656a42
alagha-go <abdihakimosman1480@gmail.com>
2022-06-01 18:58:33 +0000

    removed unneeded code

--- lib/socket/login.go
+++ lib/socket/login.go
@@ -1,13 +1,11 @@
 package socket
 
 import (
-	"errors"
 	"fmt"
 	"interphlix/lib/variables"
 	"net/http"
 
 	gosocketio "github.com/ambelovsky/gosf-socketio"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/oauth2"
 )
 
@@ -37,33 +35,4 @@ func FindChannelByIP(IP string) (*gosocketio.Channel, error) {
 		return nil, err
 	}
 	return Server.GetChannel(Channel.ID)
-}
-
-
-/// check if account is online
-func IsAccountOnline(ID primitive.ObjectID) bool {
-	for index := range Channels {
-		if Channels[index].AccountID == ID {
-			return true
-		}
-	}
-	return false
-}
-
-
-//// removes channel from the channels list
-func RemoveChannel(index int) {
-	Channels[index] = Channels[len(Channels)-1]
-    Channels = Channels[:len(Channels)-1]
-}
-
-/// function to get channel by accountID
-
-func GetChannel(AccountID primitive.ObjectID) *Channel {
-	for index := range Channels {
-		if Channels[index].AccountID == AccountID {
-			return &Channels[index]
-		}
-	}
-	return nil
 }
