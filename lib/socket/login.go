097810c27a9719e4b2c6ee95ef7d507b2e4d623a
alagha-go <abdihakimosman1480@gmail.com>
2022-06-01 08:06:47 +0000

    created function to emit Authorization token

--- lib/socket/login.go
+++ lib/socket/login.go
@@ -2,14 +2,24 @@ package socket
 
 import (
 	"fmt"
+	"interphlix/lib/variables"
+	"net/http"
 
 	gosocketio "github.com/ambelovsky/gosf-socketio"
 	"golang.org/x/oauth2"
 )
 
+/// socket.io function to get login url
 func GetUrl(channel *gosocketio.Channel) interface{} {
 	config, err := GetConfig()
 	HandlError(err)
 	url := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
 	return fmt.Sprintf(`{"url": "%s"}`, url)
+}
+
+
+/// socket.io function to get Authorization token
+func GetToken(channel *gosocketio.Channel, cookie *http.Cookie){
+	data := string(variables.JsonMarshal(cookie))
+	channel.Emit("token", data)
 }
