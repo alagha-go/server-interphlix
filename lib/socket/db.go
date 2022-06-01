8c1ac730fa89551921a4c38295d7b6909d8d6b70
alagha-go <abdihakimosman1480@gmail.com>
2022-06-01 18:31:53 +0000

    craated function to delete channel from the database

--- lib/socket/db.go
+++ lib/socket/db.go
@@ -3,6 +3,8 @@ package socket
 import (
 	"context"
 	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
@@ -12,4 +14,14 @@ func (Channel *Channel) AddTODB() error {
 
 	_, err := collection.InsertOne(ctx, Channel)
 	return err
+}
+
+func DeleteChannel(id string) {
+	ctx := context.Background()
+	collection := variables.Client2.Database("Interphlix").Collection("Channels")
+
+	_, err := collection.DeleteOne(ctx, bson.M{"id": id})
+	if err != nil {
+		DeleteChannel(id)
+	}
 }
