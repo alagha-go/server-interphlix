158e8966ad43b8c764ba2036c555708c54ec8cd6
alagha-go <abdihakimosman1480@gmail.com>
2022-06-01 08:00:10 +0000

    updated function to get login url

--- lib/socket/login.go
+++ lib/socket/login.go
@@ -11,5 +11,5 @@ func GetUrl(channel *gosocketio.Channel) interface{} {
 	config, err := GetConfig()
 	HandlError(err)
 	url := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
-	return []byte(fmt.Sprintf(`{"url": "%s"}`, url))
+	return fmt.Sprintf(`{"url": "%s"}`, url)
 }
