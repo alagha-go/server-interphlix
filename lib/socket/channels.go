195af3288974206475fa6c131c4bd54ed66c80d1
alagha-go <abdihakimosman1480@gmail.com>
2022-06-02 08:21:18 +0000

    delete channels that were disconnected by the server

---
+++ lib/socket/channels.go
@@ -0,0 +1,32 @@
+package socket
+
+import (
+	"context"
+	"interphlix/lib/socket/server"
+	"interphlix/lib/variables"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+
+
+func CleanChannelsCollection() {
+	for {
+		RemoveUnNeededChannels()
+		time.Sleep(10*time.Second)
+	}
+}
+
+
+func RemoveUnNeededChannels() {
+	Servers, _ := server.GetAllServers()
+	for _, Server := range Servers {
+		if !Server.Working {
+			ctx := context.Background()
+			collection := variables.Client2.Database("Interphlix").Collection("Channels")
+
+			collection.DeleteMany(ctx, bson.M{"server_id": Server.ID})
+		}
+	}
+}
