fc61302b8ecfdcd0b1dd3d665b25f1751f5f4f79
alagha-go <abdihakimosman1480@gmail.com>
2022-06-02 07:46:24 +0000

    created function to get server's local IP address

--- lib/socket/main.go
+++ lib/socket/main.go
@@ -1,7 +1,26 @@
 package socket
 
+import (
+	"log"
+	"net"
+)
+
 
 
 func Main() {
 	go StartSocketServer()
+	GetMyIPAddress()
+}
+
+func GetMyIPAddress() string {
+	 conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr).IP.String()
+
+    println(localAddr)
+	return localAddr
 }
