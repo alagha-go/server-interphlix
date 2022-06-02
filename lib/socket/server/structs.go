4a712efba30dee87b615fb4a6aa5df4377489ca5
alagha-go <abdihakimosman1480@gmail.com>
2022-06-02 08:00:46 +0000

    added Domain field to the Server struct

--- lib/socket/server/structs.go
+++ lib/socket/server/structs.go
@@ -5,6 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Server struct {
 	ID							primitive.ObjectID							`json:"_id,omitempty" bson:"_id,omitempty"`
+	Domain						string										`json:"domain,omitempty" bson:"domain,omitempty"`
 	IPAddress					string										`json:"ip_address,omitempty" bson:"ip_address,omitempty"`
 	LocalIPAddress				string										`json:"local_ip_address,omitempty" bson:"local_ip_address,omitempty"`
 	Secure						bool										`json:"secure,omitempty" bson:"secure,omitempty"`
