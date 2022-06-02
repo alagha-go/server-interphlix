ed49d82e0536b25a0000d54b8b5c3975c29e114a
alagha-go <abdihakimosman1480@gmail.com>
2022-06-02 07:19:00 +0000

    created server struct

---
+++ lib/socket/server/structs.go
@@ -0,0 +1,12 @@
+package server
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+
+type Server struct {
+	ID							primitive.ObjectID							`json:"_id,omitempty" bson:"_id,omitempty"`
+	IPAddress					string										`json:"ip_address,omitempty" bson:"ip_address,omitempty"`
+	LocalIPAddress				string										`json:"local_ip_address,omitempty" bson:"local_ip_address,omitempty"`
+	Secure						bool										`json:"secure,omitempty" bson:"secure,omitempty"`
+	Working						bool										`json:"working,omitempty" bson:"working,omitempty"`
+}
