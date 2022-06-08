7a6780516d2dfad2c53998ce52478ac06866fa55
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 07:50:20 +0000

    created http handler to get RatedMovies

---
+++ lib/handler/movies/movieslist.go
@@ -0,0 +1,33 @@
+package movies
+
+import (
+	"interphlix/lib/handler/accounts"
+	"interphlix/lib/movies"
+	"interphlix/lib/variables"
+	"net/http"
+)
+
+func GetWatchList(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	cookie, err := req.Cookie("token")
+	if err != nil {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	account, err := accounts.GetAccount(cookie.Value)
+	if err != nil {
+		res.WriteHeader(http.StatusNotFound)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "account does not exist"}))
+		return
+	}
+	data, status := movies.GetRatedMovies(account.ID)
+	res.WriteHeader(status)
+	res.Write(data)
+}
