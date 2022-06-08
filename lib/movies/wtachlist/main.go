44ecc5a65ebda34f7baaff6794a6979319fa7e12
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 08:22:37 +0000

    created function to load all watchlists

--- lib/movies/wtachlist/main.go
+++ lib/movies/wtachlist/main.go
@@ -1,6 +1,37 @@
 package wtachlist
 
+import (
+	"context"
+	"interphlix/lib/variables"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 
 func Main() {
-	
+
+}
+
+
+func LoadWatchList() {
+	var documents []interface{}
+	ctx := context.Background()
+	collection1 := variables.Client.Database("Interphlix").Collection("Watchlist")
+	collection := variables.Client1.Database("Interphlix").Collection("Watchlist")
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		log.Panic(err)
+	}
+	err = cursor.All(ctx, &documents)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = collection.InsertMany(ctx, documents)
+	if err != nil && err != mongo.ErrEmptySlice {
+		log.Panic(err)
+	}
 }
