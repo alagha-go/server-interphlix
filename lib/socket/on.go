86ca3a551639d531066607b6e89e7f222fd489db
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 18:02:11 +0000

    created socket.io handler to create a watchlist

---
+++ lib/socket/on.go
@@ -0,0 +1,27 @@
+package socket
+
+import (
+	"interphlix/lib/movies/watchlist"
+	"time"
+
+	gosocketio "github.com/ambelovsky/gosf-socketio"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func CreateWatchlist(channel *gosocketio.Channel, id string) interface{} {
+	Channel, err := GetChannelByIP(channel.Ip())
+	if err != nil {
+		channel.Emit("error", `{"error": "client does not exist"}`)
+		time.Sleep(500*time.Millisecond)
+		channel.Close()
+		return ""
+	}
+	var WatchList watchlist.WatchList
+	WatchList.MovieID, err = primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return `{"error": "invalid id"}`
+	}
+	WatchList.AccountID = Channel.AccountID
+	return WatchList.Create()
+}
