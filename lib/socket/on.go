01b0bf670bec2718f74017e29093fd26fea75796
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 18:13:42 +0000

    created socket.io handler to delete watchlist

--- lib/socket/on.go
+++ lib/socket/on.go
@@ -25,3 +25,21 @@ func CreateWatchlist(channel *gosocketio.Channel, id string) interface{} {
 	WatchList.AccountID = Channel.AccountID
 	return WatchList.Create()
 }
+
+
+func DeleteWatchlist(channel *gosocketio.Channel, id string) interface{} {
+	Channel, err := GetChannelByIP(channel.Ip())
+	if err != nil {
+		channel.Emit("error", `{"error": "client does not exist"}`)
+		time.Sleep(500*time.Millisecond)
+		channel.Close()
+		return ""
+	}
+	var WatchList watchlist.WatchList
+	WatchList.MovieID, err = primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return `{"error": "invalid id"}`
+	}
+	WatchList.AccountID = Channel.AccountID
+	return WatchList.Delete()
+}
