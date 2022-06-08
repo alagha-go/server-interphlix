8811e1ee5107da3274652f08d452a2b98aec2d2d
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 18:12:16 +0000

    updated delete filter

--- lib/movies/watchlist/delete.go
+++ lib/movies/watchlist/delete.go
@@ -12,7 +12,7 @@ func (WatchList *WatchList) Delete() string {
 	ctx := context.Background()
 	collection := variables.Client.Database("Interphlix").Collection("Watchlist")
 
-	_, err := collection.DeleteOne(ctx, bson.M{"_id": WatchList.ID})
+	_, err := collection.DeleteOne(ctx, bson.M{"movie_id": WatchList.MovieID, "account_id": WatchList.AccountID})
 	if err != nil {
 		variables.HandleError(err, "watchlist", "Watchlist.Delete", "could not delete watchlist")
 		return `{"error": "could not delete watch list"}`
