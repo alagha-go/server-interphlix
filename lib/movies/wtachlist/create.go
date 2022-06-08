075e7d374884e55ce7c0eb840e3b74d51dc47f50
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 08:45:55 +0000

    created function to check if a watchlist exists

--- lib/movies/wtachlist/create.go
+++ lib/movies/wtachlist/create.go
@@ -3,6 +3,8 @@ package wtachlist
 import (
 	"context"
 	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
@@ -16,4 +18,13 @@ func (WatchList *WatchList) Create() string {
 		return string(variables.JsonMarshal(variables.Error{Error: "could not create watchlist"}))
 	}
 	return `watchlist created`
+}
+
+
+func (WatchList *WatchList) Exists() bool {
+	ctx := context.Background()
+	collection := variables.Client1.Database("Interphlix").Collection("Watchlist")
+
+	err := collection.FindOne(ctx, bson.M{"account_id": WatchList.AccountID, "movie_id": WatchList.MovieID}).Decode(WatchList)
+	return err == nil
 }
