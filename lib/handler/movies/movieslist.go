187a36b9cd4e4bacd48f90a09e6add3305c08839
alagha-go <abdihakimosman1480@gmail.com>
2022-06-08 17:54:35 +0000

    created http handler to get watchlist

--- lib/handler/movies/movieslist.go
+++ lib/handler/movies/movieslist.go
@@ -31,3 +31,29 @@ func GetRatedMovies(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(status)
 	res.Write(data)
 }
+
+
+func GetMyWatchlist(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	cookie, err := req.Cookie("token")
+	if err != nil {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	account, err := accounts.GetAccount(cookie.Value)
+	if err != nil {
+		res.WriteHeader(http.StatusNotFound)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "account does not exist"}))
+		return
+	}
+	data, status := movies.GetMyWatchlist(account.ID)
+	res.WriteHeader(status)
+	res.Write(data)
+}
