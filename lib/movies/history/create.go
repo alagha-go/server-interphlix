16ee465e93517ae759e25fec0d0aac03a567d79f
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 07:44:50 +0000

    created function to create History

--- lib/movies/history/create.go
+++ lib/movies/history/create.go
@@ -3,9 +3,30 @@ package history
 import (
 	"context"
 	"interphlix/lib/variables"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+
+func (History *History) Create() string {
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("History")
+
+	History.LastTimeWatched = time.Now()
+	History.Episode.LastTimeWatched = time.Now()
+	History.Episodes = append(History.Episodes, *History.Episode)
+	History.Episode = nil
+	History.ID = primitive.NewObjectID()
+
+	_, err := collection.InsertOne(ctx, History)
+	if err != nil {
+		variables.HandleError(err, "history", "History.Create", "error while inserting document to the database")
+		return `{"error": "could not add movie to watchlist"}`
+	}
+	return "success"
+}
 
 
 func (history *History) Exists() bool {
