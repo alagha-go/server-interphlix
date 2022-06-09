fb397dd2e3494a1196369f8c945ac85a8083711d
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 18:31:42 +0000

    created function to set an account staff

--- lib/accounts/createuser.go
+++ lib/accounts/createuser.go
@@ -19,9 +19,30 @@ func CreateAccount(account Account) ([]byte, int) {
 		return variables.JsonMarshal(accountExist), http.StatusOK
 	}
 	account.ID = primitive.NewObjectID()
+	account.Type = "user"
 	_, err = collection.InsertOne(context.Background(), account)
 	if err != nil {
 		return variables.JsonMarshal(variables.Error{Error: "could not save user"}), http.StatusInternalServerError
 	}
 	return variables.JsonMarshal(account), http.StatusOK
+}
+
+
+func SetAccountStaff(ID primitive.ObjectID) ([]byte, int) {
+	ctx := context.Background()
+	collection := variables.Client.Database("Interphlix").Collection("Accounts")
+
+	filter := bson.M{"_id": bson.M{"$eq": ID}}
+	update := bson.M{
+		"$set": bson.M{
+			"type": "staff",
+		},
+	}
+
+	_, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		variables.HandleError(err, "accounts", "SetAccountStaff", "error while updating document")
+		return []byte(`{"error": "could not update account"}`), http.StatusInternalServerError
+	}
+	return []byte(`done`), http.StatusOK
 }
