9f5304d374c3d451ef6d92d0a26c7edfdb04569b
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 07:30:38 +0000

    created function to load History

--- lib/movies/history/main.go
+++ lib/movies/history/main.go
@@ -1,6 +1,38 @@
 package history
 
+import (
+	"context"
+	"interphlix/lib/variables"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 
 func Main() {
 	
+}
+
+
+func LoadHistory() {
+	var documents []interface{}
+	ctx := context.Background()
+	collection1 := variables.Client.Database("Interphlix").Collection("History")
+	collection := variables.Client1.Database("Interphlix").Collection("History")
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		log.Panic(err)
+	}
+	err = cursor.All(ctx, &documents)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	collection.Drop(ctx)
+	_, err = collection.InsertMany(ctx, documents)
+	if err != nil && err != mongo.ErrEmptySlice {
+		log.Panic(err)
+	}
 }
