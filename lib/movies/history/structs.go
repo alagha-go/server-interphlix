13efe0e22a8570f22f3e0f57de7c17d32cd5345b
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 07:32:52 +0000

    added field to both the structs

--- lib/movies/history/structs.go
+++ lib/movies/history/structs.go
@@ -1,6 +1,10 @@
 package history
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 
 type History struct {
@@ -10,10 +14,12 @@ type History struct {
 	Percentage					float64										`json:"percentage,omitempty" bson:"percentage,omitempty"`
 	Episodes					[]Episode									`json:"episodes,omitempty" bson:"episodes,omitempty"`
 	Episode						*Episode									`json:"episode,omitempty" bson:"episode,omitempty"`
+	LastTimeWatched				time.Time									`json:"last_time_watched,omitempty" bson:"last_time_watched,omitempty"`
 }
 
 type Episode struct {
 	SeasonIndex					int											`json:"season_index,omitempty" bson:"season_index,omitempty"`
 	ID							primitive.ObjectID							`json:"_id,omitempty" bson:"_id,omitempty"`
 	Percentage					float64										`json:"percentage,omitempty" bson:"percentage,omitempty"`
+	LastTimeWatched				time.Time									`json:"last_time_watched,omitempty" bson:"last_time_watched,omitempty"`
 }
