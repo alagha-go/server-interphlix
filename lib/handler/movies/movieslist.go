f2244e5f31d086e30c90f200182d3a4534de1f78
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 08:42:29 +0000

    created http handle to handle request to get History

--- lib/handler/movies/movieslist.go
+++ lib/handler/movies/movieslist.go
@@ -57,3 +57,29 @@ func GetMyWatchlist(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(status)
 	res.Write(data)
 }
+
+
+func GetMyHistory(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	valid := accounts.ValidateRequest(req)
+	if !valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	cookie, err := req.Cookie("token")
+	if err != nil {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "unauthorized"}))
+		return
+	}
+	account, err := accounts.GetAccount(cookie.Value)
+	if err != nil {
+		res.WriteHeader(http.StatusNotFound)
+		res.Write(variables.JsonMarshal(variables.Error{Error: "account does not exist"}))
+		return
+	}
+	data, status := movies.GetMyHistory(account.ID)
+	res.WriteHeader(status)
+	res.Write(data)
+}
