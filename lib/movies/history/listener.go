9ec286dc5ead47d11e69df3fd8259c0586e260e7
alagha-go <abdihakimosman1480@gmail.com>
2022-06-09 08:14:15 +0000

    listener for History collection

--- lib/movies/history/listener.go
+++ lib/movies/history/listener.go
@@ -1,5 +1,16 @@
 package history
 
+import (
+	"context"
+	"encoding/json"
+	"interphlix/lib/variables"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 
 func Listener() {
 	go ListenForHistoryCollection()
@@ -7,5 +18,32 @@ func Listener() {
 
 
 func ListenForHistoryCollection() {
-	
+	collection := variables.Client.Database("Interphlix").Collection("History")
+
+	matchPipeline := bson.D{{"$match", bson.D{{"operationType", bson.D{{ "$in", bson.A{"insert", "update", "replace"} }}}}}}
+	projectPipeline := bson.D{{ "$project", bson.D{{"fullDocument", 1}}}}
+
+	opts := options.ChangeStream()
+	opts.SetFullDocument("updateLookup")
+
+	stream, err := collection.Watch(context.TODO(), mongo.Pipeline{matchPipeline, projectPipeline}, opts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for stream.Next(context.TODO()) {
+		var data bson.M
+		if err := stream.Decode(&data); err != nil {
+			panic(err)
+		}
+		content := variables.JsonMarshal(data["fullDocument"])
+		var History History
+		json.Unmarshal(content, &History)
+		if History.Exists() {
+			History.LocalUpdate()
+		}else {
+			History.LocalCreate()
+		}
+	}
+	ListenForHistoryCollection()
 }
