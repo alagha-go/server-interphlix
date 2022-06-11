e377e7b0ada785b89ded45677c060ecab74c13e1
alagha-go <abdihakimosman1480@gmail.com>
2022-06-11 19:22:09 +0000

    created function to get home movies

--- lib/movies/recommend.go
+++ lib/movies/recommend.go
@@ -2,8 +2,11 @@ package movies
 
 import (
 	"context"
+	"interphlix/lib/movies/genres"
 	"interphlix/lib/variables"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -31,4 +34,70 @@ func GetMovies(round int) ([]byte, int) {
 	}
 
 	return variables.JsonMarshal(Movies[start:end]), http.StatusOK
+}
+
+
+func GetHome() ([]byte, int) {
+	var Movies []Movie
+	var Genres []genres.Genre
+	var recommendation Recommendation
+	recommendation.Seed = time.Now().UnixNano()
+	ctx := context.Background()
+	collection1 := variables.Client1.Database("Interphlix").Collection("Genres")
+	collection := variables.Client1.Database("Interphlix").Collection("Movies")
+	Categories := []Category{{Title: "Popular Movies", Movies: RandomMovies(recommendation.Seed, PopularMovies)}, {Title: "Popular Tvs", Movies: RandomMovies(recommendation.Seed, PopularTvShows)}}
+	recommendation.Categories = append(recommendation.Categories, Categories...)
+
+	cursor, err := collection1.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "movies", "GetHome", "error while getting genres")
+		return variables.JsonMarshal(variables.Error{Error: "could not get movies"}), http.StatusInternalServerError
+	}
+	err = cursor.All(ctx, &Genres)
+	if err != nil {
+		variables.HandleError(err, "movies", "GetHome", "error while decoding cursor")
+		return variables.JsonMarshal(variables.Error{Error: "could not get movies"}), http.StatusInternalServerError
+	}
+	
+	for index := range Genres {
+		var Category Category
+		Category.Title = Genres[index].Title
+		recommendation.Categories = append(recommendation.Categories, Category)
+	}
+
+	cursor, err = collection.Find(ctx, bson.M{})
+	if err != nil {
+		variables.HandleError(err, "movies", "GetHome", "error while getting movies")
+		return variables.JsonMarshal(variables.Error{Error: "could not get movies"}), http.StatusInternalServerError
+	}
+	err = cursor.All(ctx, &Movies)
+	if err != nil {
+		variables.HandleError(err, "movies", "GetHome", "error while decoding cursor")
+		return variables.JsonMarshal(variables.Error{Error: "could not get movies"}), http.StatusInternalServerError
+	}
+
+	for _, Movie := range Movies {
+		for _, genre := range Movie.Genres {
+			for index, category := range recommendation.Categories {
+				if genre == category.Title {
+					recommendation.Categories[index].Movies = append(recommendation.Categories[index].Movies, Movie)
+				}
+			}
+		}
+	}
+
+	for index := range recommendation.Categories {
+		if len(recommendation.Categories[index].Movies) > 20 {
+			recommendation.Categories[index].Movies = recommendation.Categories[index].Movies[:20]
+		}
+	}
+
+	return variables.JsonMarshal(recommendation), http.StatusOK
+}
+
+
+func RandomMovies(seed int64, Movies []Movie) []Movie {
+	rand.Seed(seed)
+	rand.Shuffle(len(Movies), func(i, j int) { Movies[i], Movies[j] = Movies[j], Movies[i] })
+	return Movies
 }
