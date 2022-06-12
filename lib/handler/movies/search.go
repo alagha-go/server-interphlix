3c6519e0046336844e2645f94bf319c94a30e651
alagha-go <abdihakimosman1480@gmail.com>
2022-06-12 16:35:27 +0000

    created handler for search request

---
+++ lib/handler/movies/search.go
@@ -0,0 +1,31 @@
+package movies
+
+import (
+	"interphlix/lib/handler/accounts"
+	"interphlix/lib/movies"
+	"interphlix/lib/variables"
+	"net/http"
+	"strconv"
+)
+
+
+func SearchMovies(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	err, status := accounts.ValidateRequest(req, "user")
+	if err != nil {
+		res.WriteHeader(status)
+		res.Write(variables.JsonMarshal(variables.Error{Error: err.Error()}))
+		return
+	}
+	Type := req.URL.Query().Get("type")
+	query := req.URL.Query().Get("query")
+	Genre := req.URL.Query().Get("genre")
+	round, err := strconv.Atoi(req.URL.Query().Get("round"))
+	if err != nil {
+		round = 0
+	}
+	
+	data, status := movies.SearchMovies(query, Type, Genre, round)
+	res.WriteHeader(status)
+	res.Write(data)
+}
