68e12131e56bf61a294ed4a371b36192781bd710
alagha-go <abdihakimosman1480@gmail.com>
2022-06-12 08:43:44 +0000

    created function to get featured movies

--- lib/movies/getfeaturedmovies.go
+++ lib/movies/getfeaturedmovies.go
@@ -1,5 +1,12 @@
 package movies
 
+import (
+	"context"
+	"interphlix/lib/variables"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 
 
 var (
@@ -10,4 +17,22 @@ var (
 func CollectMovies() {
 	SetPopularMovies()
 	SetPopularTvShows()
+}
+
+
+func GetFeaturedMovies(seed int64) ([]Movie, error) {
+	var Movies []Movie
+	ctx := context.Background()
+	collection := variables.Client1.Database("Interphlix").Collection("Movies")
+
+	cursor, err := collection.Find(ctx, bson.M{"featured": true})
+	if err != nil {
+		variables.HandleError(err, "movies", "GetPopularMovies", "error while getting data from the database")
+	}
+	cursor.All(ctx, &Movies)
+	Movies = RandomMovies(seed, Movies)
+	if len(Movies) > 5 {
+		return Movies[:5], nil
+	}
+	return Movies, nil
 }
