3751513a99f7f328550a9ab0432e6d29b1111691
alagha-go <abdihakimosman1480@gmail.com>
2022-06-12 16:39:36 +0000

    req handler for serch catsts

--- lib/handler/movies/search.go
+++ lib/handler/movies/search.go
@@ -3,6 +3,7 @@ package movies
 import (
 	"interphlix/lib/handler/accounts"
 	"interphlix/lib/movies"
+	"interphlix/lib/movies/casts"
 	"interphlix/lib/variables"
 	"net/http"
 	"strconv"
@@ -26,6 +27,25 @@ func SearchMovies(res http.ResponseWriter, req *http.Request) {
 	}
 	
 	data, status := movies.SearchMovies(query, Type, Genre, round)
+	res.WriteHeader(status)
+	res.Write(data)
+}
+
+
+func SearchCast(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("content-type", "application/json")
+	err, status := accounts.ValidateRequest(req, "user")
+	if err != nil {
+		res.WriteHeader(status)
+		res.Write(variables.JsonMarshal(variables.Error{Error: err.Error()}))
+		return
+	}
+	query := req.URL.Query().Get("query")
+	round, err := strconv.Atoi(req.URL.Query().Get("round"))
+	if err != nil {
+		round = 0
+	}
+	data, status := casts.SearchCasts(query, round)
 	res.WriteHeader(status)
 	res.Write(data)
 }
