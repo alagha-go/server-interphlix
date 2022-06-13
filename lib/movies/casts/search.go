ae1bcb1f8569b8005d3af8593f08ca76fc035ba9
alagha-go <abdihakimosman1480@gmail.com>
2022-06-13 08:02:25 +0000

    updated function to search for casts

--- lib/movies/casts/search.go
+++ lib/movies/casts/search.go
@@ -8,38 +8,40 @@ import (
 	"github.com/blevesearch/bleve/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
 func SearchCasts(querry string, round int) ([]byte, int) {
-	var length int = 50
+	start := 0
+	length := 50
 	if round != 0 {
+		if round == 1{
+			start = 50
+		}else {
+			start = ((round-1)*20) + 50
+		}
 		length = (round*20) + 50
 	}
 	var Casts []Cast
 	ctx := context.Background()
 	collection := variables.Client1.Database("Interphlix").Collection("Casts")
 
-	query := bleve.NewQueryStringQuery(querry)
-	searchRequest := bleve.NewSearchRequest(query)
-	searchResult, err := Index.Search(searchRequest)
+	sort := bson.D{{"score", bson.D{{"$meta", "textScore"}}}}
+	projection := bson.D{{"score", bson.D{{"$meta", "textScore"}}}}
+	opts := options.Find().SetSort(sort).SetProjection(projection)
+
+	cursor, err := collection.Find(ctx, bson.M{"$text": bson.M{"$search": querry}}, opts)
 	if err != nil {
-		variables.HandleError(err, "casts", "SearchCasts", "error while searching data")
 		return variables.JsonMarshal(variables.Error{Error: "could not search data"}), http.StatusInternalServerError
 	}
+	cursor.All(ctx, &Casts)
 
-	for _, Hit := range searchResult.Hits {
-		var Cast Cast
-		ID, _ := primitive.ObjectIDFromHex(Hit.ID)
-		err := collection.FindOne(ctx, bson.M{"_id": ID}).Decode(&Cast)
-		if err == nil {
-			Casts = append(Casts, Cast)
-		}
+	if start > len(Casts) {
+		return variables.JsonMarshal(Casts[0:0]), http.StatusOK
+	}else if length > len(Casts) {
+		return variables.JsonMarshal(Casts[start:]), http.StatusOK
 	}
 
-	if len(Casts) < length {
-		return variables.JsonMarshal(Casts), http.StatusOK
-	}
-
-	return variables.JsonMarshal(Casts[:length]), http.StatusOK
+	return variables.JsonMarshal(Casts[start:length]), http.StatusOK
 }
