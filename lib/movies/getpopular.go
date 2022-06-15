4bfa115a5ae57d4d6ff3c323638d3b39be9c38c8
alagha-go <abdihakimosman1480@gmail.com>
2022-06-15 16:57:20 +0000

    craeted function to get popular movies

---
+++ lib/movies/getpopular.go
@@ -0,0 +1,28 @@
+package movies
+
+import (
+	"interphlix/lib/variables"
+	"net/http"
+)
+
+
+func GetPoPularMovies(seed int64, round int) ([]byte, int) {
+	start := 0
+	end := 20;
+	if round != 0 {
+		start = (round*20) + 1
+		end = (round*20) + 21
+	}
+	Movies, err := GetPopularMovies()
+	if err != nil {
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
+	}
+
+	if start > len(Movies) {
+		return variables.JsonMarshal([]Movie{}), http.StatusOK
+	}else if end > len(Movies) {
+		return variables.JsonMarshal(Movies[start:]), http.StatusOK
+	}
+
+	return variables.JsonMarshal(Movies[start:end]), http.StatusOK
+}
