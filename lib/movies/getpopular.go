99024f657c43108890c3f04c653da05d74efd2ec
alagha-go <abdihakimosman1480@gmail.com>
2022-06-15 17:01:13 +0000

    created function to get featured movies

--- lib/movies/getpopular.go
+++ lib/movies/getpopular.go
@@ -17,6 +17,7 @@ func GetPoPularMovies(seed int64, round int) ([]byte, int) {
 	if err != nil {
 		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
+	Movies = RandomMovies(seed, Movies)
 
 	if start > len(Movies) {
 		return variables.JsonMarshal([]Movie{}), http.StatusOK
@@ -36,6 +37,29 @@ func GetPoPularTvShows(seed int64, round int) ([]byte, int) {
 		end = (round*20) + 21
 	}
 	Movies, err := GetPopularTvShows()
+	if err != nil {
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
+	}
+	Movies = RandomMovies(seed, Movies)
+
+	if start > len(Movies) {
+		return variables.JsonMarshal([]Movie{}), http.StatusOK
+	}else if end > len(Movies) {
+		return variables.JsonMarshal(Movies[start:]), http.StatusOK
+	}
+
+	return variables.JsonMarshal(Movies[start:end]), http.StatusOK
+}
+
+
+func GetFeatured(seed int64, round int) ([]byte, int) {
+	start := 0
+	end := 20;
+	if round != 0 {
+		start = (round*20) + 1
+		end = (round*20) + 21
+	}
+	Movies, err := GetFeaturedMovies(seed)
 	if err != nil {
 		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
 	}
