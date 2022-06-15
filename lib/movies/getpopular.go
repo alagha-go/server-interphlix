8b7e8ba48545c20fdf1f1678f7fdfcf21ac77d53
alagha-go <abdihakimosman1480@gmail.com>
2022-06-15 16:58:51 +0000

    created function to get popular tvshows

--- lib/movies/getpopular.go
+++ lib/movies/getpopular.go
@@ -26,3 +26,25 @@ func GetPoPularMovies(seed int64, round int) ([]byte, int) {
 
 	return variables.JsonMarshal(Movies[start:end]), http.StatusOK
 }
+
+
+func GetPoPularTvShows(seed int64, round int) ([]byte, int) {
+	start := 0
+	end := 20;
+	if round != 0 {
+		start = (round*20) + 1
+		end = (round*20) + 21
+	}
+	Movies, err := GetPopularTvShows()
+	if err != nil {
+		return variables.JsonMarshal(variables.Error{Error: err.Error()}), http.StatusInternalServerError
+	}
+
+	if start > len(Movies) {
+		return variables.JsonMarshal([]Movie{}), http.StatusOK
+	}else if end > len(Movies) {
+		return variables.JsonMarshal(Movies[start:]), http.StatusOK
+	}
+
+	return variables.JsonMarshal(Movies[start:end]), http.StatusOK
+}
